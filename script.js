486f7ec028002b8d9d9d35373602e25b31151adf
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-01-30 13:44:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function decimalToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
+const romanNumeralMap = [
+  { decimal: 1000, roman: 'M' },
+  { decimal: 900, roman: 'CM' },
+  { decimal: 500, roman: 'D' },
+  { decimal: 400, roman: 'CD' },
+  { decimal: 100, roman: 'C' },
+  { decimal: 90, roman: 'XC' },
+  { decimal: 50, roman: 'L' },
+  { decimal: 40, roman: 'XL' },
+  { decimal: 10, roman: 'X' },
+  { decimal: 9, roman: 'IX' },
+  { decimal: 5, roman: 'V' },
+  { decimal: 4, roman: 'IV' },
+  { decimal: 1, roman: 'I' },
+];
 
 function decimalToRoman(decimalNum) {
   let romanNumeral = '';
@@ -24,15 +27,6 @@ function decimalToRoman(decimalNum) {
 
 const decimalNum = prompt();
 const romanNumeral = decimalToRoman(decimalNum);
-console.log(`${romanNumeral}`);  
+console.log(`${romanNumeral}`);
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
 module.exports = decimalToRoman
