6969e447938636b23f5273210a36c2cf4400d31a
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-01-30 13:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,62 +10,31 @@ function convertToRoman(num) {
     };
 
   //your code here
-	 let result = "";
-   while(num){
-      if(num>=1000){
-         result += "M";
-         num -= 1000;
-      }else if(num>=500){
-         if(num>=900){
-            result += "CM";
-            num -= 900;
-         }else{
-            result += "D";
-            num -= 500;
-         }
-         }else if(num>=100){
-            if(num>=400){
-               result += "CD";
-               num -= 400;
-            }else{
-               result += "C";
-               num -= 100;
-            }
-         }else if(num>=50){
-            if(num>=90){
-               result += "XC";
-               num -= 90;
-            }else{
-               result += "L";
-               num -= 50;
-            }
-         }else if(num>=10){
-            if(num>=40){
-               result += "XL";
-               num -= 40;
-            }else{
-               result += "X";
-               num -= 10;
-            }
-         }else if(num>=5){
-            if(num>=9){
-               result += "IX";
-               num -= 9;
-           }else{
-               result += "V";
-               num -= 5;
-            }
-         }else{
-            if(num>=4){
-               result += "IV";
-               num -= 4;
-         }else{
-            result += "I";
-            num -= 1;
-         }
-      }
-   }
-   return result;
+function decimalToRoman(decimalNum) {
+  const romanNumeralMap = [
+    { decimal: 1000, roman: 'M' },
+    { decimal: 900, roman: 'CM' },
+    { decimal: 500, roman: 'D' },
+    { decimal: 400, roman: 'CD' },
+    { decimal: 100, roman: 'C' },
+    { decimal: 90, roman: 'XC' },
+    { decimal: 50, roman: 'L' },
+    { decimal: 40, roman: 'XL' },
+    { decimal: 10, roman: 'X' },
+    { decimal: 9, roman: 'IX' },
+    { decimal: 5, roman: 'V' },
+    { decimal: 4, roman: 'IV' },
+    { decimal: 1, roman: 'I' },
+  ];
+
+  let romanNumeral = '';
+  for (let i = 0; i < romanNumeralMap.length; i++) {
+    while (decimalNum >= romanNumeralMap[i].decimal) {
+      romanNumeral += romanNumeralMap[i].roman;
+      decimalNum -= romanNumeralMap[i].decimal;
+    }
+  }
+  return romanNumeral;
 }
 
 }
