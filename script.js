387b539c2a8f30135bfb713e9cdd6a5c93b0a46d
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-09 11:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-const romanNumeralMap = [
-  { decimal: 1000, roman: 'M' },
-  { decimal: 900, roman: 'CM' },
-  { decimal: 500, roman: 'D' },
-  { decimal: 400, roman: 'CD' },
-  { decimal: 100, roman: 'C' },
-  { decimal: 90, roman: 'XC' },
-  { decimal: 50, roman: 'L' },
-  { decimal: 40, roman: 'XL' },
-  { decimal: 10, roman: 'X' },
-  { decimal: 9, roman: 'IX' },
-  { decimal: 5, roman: 'V' },
-  { decimal: 4, roman: 'IV' },
-  { decimal: 1, roman: 'I' },
-];
+function convertToRoman(num) {
+  var romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  var result = "";
 
-function decimalToRoman(decimalNum) {
-  let romanNumeral = '';
-  for (let i = 0; i < romanNumeralMap.length; i++) {
-    while (decimalNum >= romanNumeralMap[i].decimal) {
-      romanNumeral += romanNumeralMap[i].roman;
-      decimalNum -= romanNumeralMap[i].decimal;
+  for (var key in romanNumerals) {
+    while (num >= romanNumerals[key]) {
+      result += key;
+      num -= romanNumerals[key];
     }
   }
-  return romanNumeral;
+  return result;
 }
-
-const decimalNum = prompt();
-const romanNumeral = decimalToRoman(decimalNum);
-console.log(`${romanNumeral}`);
-
-module.exports = decimalToRoman
